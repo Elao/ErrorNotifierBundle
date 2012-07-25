0306c4726a39ba8a4440909d2bfb3ceabecbbddb
SCS <hpmewes@gmx.de>
2012-07-25 15:49:28 +0300

    logger and status code can now used in template

--- Listener/Notifier.php
+++ Listener/Notifier.php
@@ -2,12 +2,12 @@
 
 namespace Elao\ErrorNotifierBundle\Listener;
 
-use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
 use \Swift_Mailer;
-
 use Symfony\Component\Templating\EngineInterface;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 
 /**
  * Notifier
@@ -36,22 +36,20 @@ class Notifier
      *
      * @param Swift_Mailer    $mailer     mailer
      * @param EngineInterface $templating templating
+     * @param LoggerInterface $logger
      * @param string          $from       send mail from
      * @param string          $to         send mail to
      * @param boolean         $handle404  handle 404 error ?
      *
      * @return void
      */
-    public function __construct(Swift_Mailer $mailer, EngineInterface $templating, $from, $to, $handle404 = false)
-    {
+    public function __construct(Swift_Mailer $mailer, EngineInterface $templating, $from, $to, $handle404 = false, DebugLoggerInterface $logger = null) {
         $this->mailer = $mailer;
-
         $this->templating = $templating;
-
+        $this->logger = $logger;
+        
         $this->from = $from;
-
         $this->to = $to;
-
         $this->handle404 = $handle404;
     }
 
@@ -72,10 +70,14 @@ class Notifier
 
         if ($exception instanceof HttpException) {
             if (500 === $exception->getStatusCode() || (404 === $exception->getStatusCode() && true === $this->handle404)) {
+                $logger = $this->logger instanceof DebugLoggerInterface ? $this->logger : null;
+
                 $body = $this->templating->render('ElaoErrorNotifierBundle::mail.html.twig', array(
                     'exception'       => $exception,
                     'exception_class' => get_class($exception),
                     'request'         => $event->getRequest(),
+                    'logger'          => $logger,
+                    'status_code'     => $exception->getStatusCode()
                 ));
 
                 $mail = \Swift_Message::newInstance()
